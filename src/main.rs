16a28fcfa9b1d0a8141240cfc651e75f8c900a8b
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-09 22:08:49 -0400

    input body

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nGuess the number!");
+
+    println!("\nPlease input your guess.");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("\nGuess another number. ");
+    let mut guess_again = String::new();
+
+    io::stdin()
+        .read_line(&mut guess_again)
+        .expect("Failed to read line");
+
+    println!("\nYour first guess was {guess}and your second guess was {guess_again}");
 }
