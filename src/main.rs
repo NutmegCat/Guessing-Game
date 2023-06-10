06510160e0431debca45f9d9eca023aa826154f2
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-09 23:55:11 -0400

    guessing feature working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("\nGuess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("\nPlease input your guess.");
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("\nFailed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     
-    print!("\nYou guessed: {}\n", guess);   
+    println!("\nYou guessed: {guess}\n");
+ 
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!\n"),
+        Ordering::Greater => println!("Too big!\n"),
+        Ordering::Equal => println!("You win!\n"),
+    }
 }
