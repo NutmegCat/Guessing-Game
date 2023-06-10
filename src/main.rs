ce6eca0d8a806271f3f0b8ccffdc1beb71117427
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-09 22:19:19 -0400

    master

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use std::io;
-
 fn main() {
-    println!("\nGuess the number!");
-
-    println!("\nPlease input your guess.");
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    println!("\nGuess another number. ");
-    let mut guess_again = String::new();
-
-    io::stdin()
-        .read_line(&mut guess_again)
-        .expect("Failed to read line");
-
-    println!("\nYour first guess was {guess}and your second guess was {guess_again}");
-    
+    println!("test");    
 }
